fdc0f705adcedb110a695c0e62ecaccab2fec95b
Dominika <01168920@pw.edu.pl>
2023-10-27 23:27:12 +0200

    optimalization

--- gradient.py
+++ gradient.py
@@ -23,20 +23,27 @@ class params_t:
 
 
 def make_plot(result):
-    # styles = ['-o', '-x', '--']
+    names = ["max iteration = 50",
+             "max iteration = 100",
+             "max iteration = 250",
+             "max iteration = 1000"]
     for idx, result in enumerate(results):
         plt.plot(
             [i for i in range(0, result.num_of_iterations + 1)],
             result.min_func_value,
-            label=f"Result {idx+1}",
+            label=names[idx],
         )
+    # plt.plot(
+    #     [i for i in range(0, result[0].num_of_iterations + 1)],
+    #     result[0].min_func_value)
 
     # plt.yscale("log")
     plt.xlabel("Iteration")
     plt.ylabel("Value of the objective function")
     plt.title("Plot of the objective function values from iterations")
-    plt.title(f"Alpha = 1, learning rate = 0.001, max iteration = 100")
+    plt.title(f"α = 1, epsilon = 1e-6, β = 0.1")
     plt.legend()
+    plt.savefig("a1_rozne_iter.png")
     plt.show()
 
 
@@ -50,8 +57,6 @@ def objective_function(x, alpha):
     return x0.sum()
 
 
-# objective_functions = [partial(objective_function, alpha=1.0), partial(
-#     objective_function, alpha=10.0)], partial(objective_function, alpha=100.0)
 objective_function_1 = partial(objective_function, alpha=1.0)
 objective_function_10 = partial(objective_function, alpha=10.0)
 objective_function_100 = partial(objective_function, alpha=100.0)
@@ -60,41 +65,44 @@ objective_function_100 = partial(objective_function, alpha=100.0)
 def gradient_descent(new_objective_function, x0, params) -> optim_result_t:
     x = x0.copy()
     obj_func_values = []
-    for i in range(params.max_iter):
-        obj_func_values.append(new_objective_function(x=x))
-        gradient_func = grad(new_objective_function)
-        gradient = gradient_func(x)
-        if np.linalg.norm(gradient) <= params.epsilon:
-            break
-        x = x - params.learning_rate * gradient
-        if (
-            params.warunek1 == 1
-            and np.max(np.abs(params.learning_rate * gradient)) < params.epsilon
-        ):
-            break
-    return optim_result_t(x, obj_func_values, i)
+    if params.warunek3 == 1:
+        for i in range(params.max_iter):
+            obj_func_values.append(new_objective_function(x=x))
+            gradient_func = grad(new_objective_function)
+            gradient = gradient_func(x)
+            if np.linalg.norm(gradient) <= params.epsilon and params.warunek2 == 1:
+                break
+            x = x - params.learning_rate * gradient
+            if (
+                params.warunek1 == 1
+                and np.max(np.abs(params.learning_rate * gradient)) < params.epsilon
+            ):
+                break
+        return optim_result_t(x, obj_func_values, i)
+    else:
+        return optim_result_t(x, obj_func_values, 0)
 
 
 x0 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
 params = [
+    params_t(0.1, 1e-6, 10, 1, 1, 1),
+    params_t(0.1, 1e-6, 50, 1, 1, 1),
     params_t(0.1, 1e-6, 100, 1, 1, 1),
-    params_t(0.01, 1e-6, 100, 1, 1, 1),
-    params_t(0.001, 1e-6, 100, 1, 1, 1)
+    params_t(0.1, 1e-6, 500, 1, 1, 1)
 ]
 results = [
-    # gradient_descent(objective_functions[0], x0, params[0]),
-    # gradient_descent(objective_functions[1], x0, params[1]),
-    # gradient_descent(objective_functions[2], x0, params[2]),
-
     gradient_descent(objective_function_1, x0, params[0]),
-    gradient_descent(objective_function_10, x0, params[1]),
-    gradient_descent(objective_function_100, x0, params[2]),
+    gradient_descent(objective_function_1, x0, params[1]),
+    gradient_descent(objective_function_1, x0, params[2]),
+    gradient_descent(objective_function_1, x0, params[3]),
 ]
+
+make_plot(results)
+
 
 # print(f"Minimum found: {results[0].minimum}")
 # print(
 #     f"Value of the objective function - minimum: {objective_function_1(results[0].minimum)}"
 # )
 # print(f"Number of iterations: {results[0].num_of_iterations}")
-
-make_plot(results)
--- mimi.py
+++
@@ -1,9 +0,0 @@
-tekst = input("Ty + Ja = Nachosy i sosik serowy? (odpowiedź NIE lub TAK)")
-if tekst == "NIE" or tekst == "nie":
-    print(":(((( ")
-    tekst = input("Odpowiedz poprawnie <angy>: ")
-    tekst = input("Teraz czy po PUST? ")
-    print("Jupi! :)")
-else:
-    tekst = input("Teraz czy po PUST? ")
-    print("Jupi! :)")
